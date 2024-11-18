2e79a6cbdec147d38e0b0db3b444f6e0aeebc0c7
lorenzo0932 <60674575+lorenzo0932@users.noreply.github.com>
2024-11-18 09:38:44 +0100

    Aggionato script per liberare la ram

--- aux/FreeMemoryFromLLMs.sh
+++ aux/FreeMemoryFromLLMs.sh
@@ -3,6 +3,7 @@
 #riavvio i container che mi servono rispettivamente per ollama e open-webui e watchtower
 docker restart ollama
 docker restart open-webui
+lms unload --all
 #docker restart watchtower
     
 
